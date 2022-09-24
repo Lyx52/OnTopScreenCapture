fa40e72e0a38beaf281a0c8005f40d6d4695cb7b
Ikars <lyxikars123@gmail.com>
2022-09-24 22:56:31 +0300

    Initial commit

---
+++ ScreenCapture/Capture/CaptureCompositor.cs
@@ -0,0 +1,91 @@
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using Composition.WindowsRuntimeHelpers;
+using System;
+using System.Numerics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX.Direct3D11;
+using Windows.UI.Composition;
+
+namespace OnTopCapture.Capture
+{
+    public class CaptureCompositor : IDisposable
+    {
+        private Compositor ContentCompositor;
+        private ContainerVisual RootContainer;
+
+        private SpriteVisual ContentSprite;
+        private CompositionSurfaceBrush ContentBrush;
+
+        private IDirect3DDevice GraphicsDevice;
+        private ScreenCapture Capture;
+
+        public CaptureCompositor(Compositor c)
+        {
+            ContentCompositor = c;
+            GraphicsDevice = Direct3D11Helper.CreateDevice();
+
+            // Setup the root.
+            RootContainer = ContentCompositor.CreateContainerVisual();
+            RootContainer.RelativeSizeAdjustment = Vector2.One;
+
+            // Setup the content.
+            ContentBrush = ContentCompositor.CreateSurfaceBrush();
+            ContentBrush.Stretch = CompositionStretch.Fill;
+            ContentSprite = ContentCompositor.CreateSpriteVisual();
+            ContentSprite.RelativeSizeAdjustment = Vector2.One;
+            ContentSprite.Brush = ContentBrush;
+            //content.Shadow = shadow;
+            RootContainer.Children.InsertAtTop(ContentSprite);
+        }
+
+        public Visual Visual => RootContainer;
+
+        public void Dispose()
+        {
+            StopCapture();
+            ContentCompositor = null;
+            RootContainer.Dispose();
+            ContentSprite.Dispose();
+            ContentBrush.Dispose();
+            GraphicsDevice.Dispose();
+        }
+
+        public void StartCaptureFromItem(GraphicsCaptureItem item)
+        {
+            StopCapture();
+            Capture = new ScreenCapture(GraphicsDevice, item);
+            ContentBrush.Surface = Capture.CreateSurface(ContentCompositor);
+
+            Capture.StartCapture();
+        }
+
+        public void StopCapture()
+        {
+            Capture?.Dispose();
+            ContentBrush.Surface = null;
+        }
+    }
+}
---
+++ ScreenCapture/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using OnTopCapture.Capture;
+using Composition.WindowsRuntimeHelpers;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using Windows.Foundation.Metadata;
+using Windows.Graphics.Capture;
+using Windows.UI.Composition;
+using System.Runtime.InteropServices;
+
+namespace OnTopCapture
+{
+
+    public partial class MainWindow : Window
+    {
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr handle, int nCmdShow);
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        private static extern bool IsIconic(IntPtr handle);
+        private IntPtr WindowHandle;
+        private Compositor WindowCompositor;
+        private CompositionTarget TargetComposition;
+        private ContainerVisual Root;
+        private CaptureCompositor Compositor;
+        private ObservableCollection<Process> Processes;
+        private bool mIsOnTop = false;
+        private bool mIsCapturing = false;
+        public bool IsCapturing
+        {
+            get => mIsCapturing;
+            set
+            {
+                mIsCapturing = value;
+                StopCaptureButton.IsEnabled = value;
+                PrimaryMonitorCaptureButton.IsEnabled = !value;
+            }
+        }
+        public bool IsOnTop
+        {
+            get => mIsOnTop;
+            set
+            {
+                mIsOnTop = value;
+                this.Topmost = value;
+                if (mIsOnTop)
+                {
+                    this.Activate();
+                }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var interopWindow = new WindowInteropHelper(this);
+            WindowHandle = interopWindow.Handle;
+            InitComposition();
+            QueryWindows();
+        }
+
+        private void InitComposition()
+        {
+            // Create the compositor.
+            WindowCompositor = new Compositor();
+
+            // Create a target for the window.
+            TargetComposition = WindowCompositor.CreateDesktopWindowTarget(WindowHandle, true);
+
+            // Attach the root visual.
+            Root = WindowCompositor.CreateContainerVisual();
+            Root.RelativeSizeAdjustment = Vector2.One;
+            TargetComposition.Root = Root;
+
+            // Setup the rest of the sample application.
+            Compositor = new CaptureCompositor(WindowCompositor);
+            Root.Children.InsertAtTop(Compositor.Visual);
+        }
+
+        private void QueryWindows()
+        {
+            if (ApiInformation.IsApiContractPresent(typeof(Windows.Foundation.UniversalApiContract).FullName, 8))
+            {
+                var processses = Process.GetProcesses().Where((process) =>
+                {
+                    if (string.IsNullOrWhiteSpace(process.MainWindowTitle) || process.MainWindowHandle == WindowHandle)
+                        return false;
+                    return WindowEnumerationHelper.IsWindowValidForCapture(process.MainWindowHandle);
+                }).ToList();
+                Processes = new ObservableCollection<Process>(processses);
+                ProcessCaptureList.Items.Clear();
+                foreach (Process p in Processes)
+                {
+                    var item = new MenuItem { Header = $"{p.MainWindowTitle} ({p.ProcessName} - {p.Id})" };
+                    item.Click += ((s, e) => {
+                        if (IsIconic(p.MainWindowHandle))
+                        {
+                            ShowWindow(p.MainWindowHandle, 9); // SW_RESTORE
+                            this.Activate(); // Restore ontop app as topmost
+                        }
+                        this.StartHwndCapture(p.MainWindowHandle);
+                    });
+                    ProcessCaptureList.Items.Add(item);
+                }
+            }
+        }
+        private void StartHwndCapture(IntPtr hwnd)
+        {
+            GraphicsCaptureItem item = CaptureHelper.CreateItemForWindow(hwnd);
+            if (item != null)
+            {
+                Compositor.StartCaptureFromItem(item);
+                this.IsCapturing = true;
+            }
+        }
+
+        private void StartHmonCapture(IntPtr hmon)
+        {
+            GraphicsCaptureItem item = CaptureHelper.CreateItemForMonitor(hmon);
+            if (item != null)
+            {
+                Compositor.StartCaptureFromItem(item);
+            }
+        }
+
+        private void StartPrimaryMonitorCapture()
+        {
+            MonitorInfo monitor = (from m in MonitorEnumerationHelper.GetMonitors()
+                           where m.IsPrimary
+                           select m).First();
+            StartHmonCapture(monitor.Hmon);
+        }
+
+        private void StopCapture()
+        {
+            this.IsCapturing = false;
+            Compositor.StopCapture();
+        }
+        private void ProcessList_Click(object sender, RoutedEventArgs e)
+        {
+            this.QueryWindows();
+        }
+        private void WindowOnTop_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsOnTop = WindowOnTopButton.IsChecked;
+        }
+        private void PrimaryMonitorCapture_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsCapturing = true;
+            this.StartPrimaryMonitorCapture();
+        }
+
+        
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.StopCapture();
+            Application.Current.Shutdown();
+        }
+        private void StopCapturing_Click(object sender, RoutedEventArgs e)
+        {
+            this.StopCapture();
+        }
+    }
+}
